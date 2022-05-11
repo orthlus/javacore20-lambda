9c1d55342927d77b1a17d03ee5eb07af8d0e46bf
Шапран Константин <konstantin.shapran@domrf.ru>
2022-05-11 18:03:44 +0300

    lambda/task 1 - fix

--- src/lambda/task1/Calculator.java
+++ src/lambda/task1/Calculator.java
@@ -7,7 +7,13 @@ public class Calculator {
 	BinaryOperator<Integer> plus = (x, y) -> x + y;
 	BinaryOperator<Integer> minus = (x, y) -> x - y;
 	BinaryOperator<Integer> multiply = (x, y) -> x * y;
-	BinaryOperator<Integer> divide = (x, y) -> x / y;
+	BinaryOperator<Integer> divide = (x, y) -> {
+		if (y == 0) {
+			System.out.println("Попытка деления на 0, возвращён делитель");
+			return x;
+		}
+		return x / y;
+	};
 	UnaryOperator<Integer> pow = x -> x * x;
 	UnaryOperator<Integer> abs = x -> x > 0 ? x:-x;
 	Predicate<Integer> isPositive = x -> x > 0;
@@ -17,9 +23,6 @@ public class Calculator {
 		Calculator calc = Calculator.instance.get();
 		int a = calc.plus.apply(1, 2);
 		int b = calc.minus.apply(1, 1);
-		// ошибка деления на 0, решение - не делить на 0
-		// int c = calc.divide.apply(a, b);
-		b = calc.plus.apply(b, 1);
 		int c = calc.divide.apply(a, b);
 		calc.println.accept(c);
 	}
